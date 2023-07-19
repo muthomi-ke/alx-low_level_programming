d8f2ff66cb2dc9fac06255f085e78d7a40cc059a
Muthomi <rohimuthomi@gmail.com>
2023-07-19 15:53:29 -0700

    kloser

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers together
+ * @a: value to be used for addition
+ * @b: second value to be used for addition
+ * Return: int (Sum)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
